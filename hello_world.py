29d4883373d593a09622799638b658b8e4b069f1
shiz5198 <shiz5198@gmail.com>
2014-01-22 21:08:13 -0500

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+# This is my first python program!
+
+having = "Hello"
+fun = "World"
+With = '!'
+python = '\a'
+
+print(having, fun, With, python)
+game = input("What's your favorite game? \n")
+
+
+while game != "skyrim":
+ game = input("Really? Are you sure? Maybe you should try again. \n")
+
+  
+print("Really?", game.upper(), " is an awesome game! But my favorite game is 三国志！")
+print("It prints Chinese, Nice!")
+print("""
+┈┈┈┈┈┈▕▔╲
+┈┈┈┈┈┈┈▏▕
+┈┈┈┈┈┈┈▏▕▂▂▂
+▂▂▂▂▂▂╱┈▕▂▂▂▏
+▉▉▉▉▉┈┈┈▕▂▂▂▏
+▉▉▉▉▉┈┈┈▕▂▂▂▏
+▔▔▔▔▔▔╲▂▕▂▂▂I
+""")
+input("\n\nPress the Enter key to exit.")
