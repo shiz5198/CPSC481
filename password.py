0474b2d14ffb87a3291ffd524cc3cb81019d81ac
shiz5198 <shiz5198@gmail.com>
2014-02-20 02:10:52 -0500

    Create password.py

---
+++ password.py
@@ -0,0 +1,13 @@
+# Password
+# Demonstrates the if statement
+
+print("Welcome to System Security Inc.")
+print("-- where security is our middle name\n")
+password = input("Enter your password: ")
+
+if password == "secret":
+ print("Access Granted")
+else:
+ print("Access Denied")
+ 
+input("\n\nPress the enter key to exit.")
